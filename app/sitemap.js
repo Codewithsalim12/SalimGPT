33ad2b993501e1ef7166128cffa5e31bda141cc0
Mohammad salim <mohammadsalimmir13@gmail.com>
2025-02-25 20:46:07 +0530

    Fixed sitemap issue

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,17 +1,40 @@
-export default function sitemap() {
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://salimgpt.onrender.com/</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <priority>1.0</priority>
-      </url>
-    </urlset>`,
+export async function GET() {
+  const urls = [
     {
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    }
-  );
+      loc: "https://salimgpt.onrender.com/",
+      lastmod: new Date().toISOString(),
+      priority: 1.0,
+    },
+    {
+      loc: "https://salimgpt.onrender.com/about",
+      lastmod: new Date().toISOString(),
+      priority: 0.8,
+    },
+    {
+      loc: "https://salimgpt.onrender.com/contact",
+      lastmod: new Date().toISOString(),
+      priority: 0.7,
+    },
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+      .map(
+        (url) => `
+      <url>
+        <loc>${url.loc}</loc>
+        <lastmod>${url.lastmod}</lastmod>
+        <priority>${url.priority}</priority>
+      </url>
+    `
+      )
+      .join("")}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
