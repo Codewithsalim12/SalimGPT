b48fae2fb2c8d9476d3f78495df10abbab1a869d
Mohammad salim <mohammadsalimmir13@gmail.com>
2025-02-26 16:38:03 +0530

    fixed sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,27 +1,23 @@
 export async function GET() {
   const baseUrl = "https://salimgpt.onrender.com";
 
-  // Define your API endpoints that should be indexed
   const routes = [
-    "/api/route", // Example API route
+    { loc: `${baseUrl}/`, priority: 1.0 },
+    { loc: `${baseUrl}/about`, priority: 0.8 },
+    { loc: `${baseUrl}/contact`, priority: 0.7 },
   ];
 
-  // Generate XML format
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${routes
-      .map((route) => {
-        return `
-        <url>
-          <loc>${baseUrl}${route}</loc>
-        </url>`;
-      })
-      .join("")}
-  </urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${routes
+        .map(
+          (route) =>
+            `<url><loc>${route.loc}</loc><priority>${route.priority}</priority></url>`
+        )
+        .join("")}
+    </urlset>`;
 
   return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
+    headers: { "Content-Type": "application/xml" },
   });
 }
